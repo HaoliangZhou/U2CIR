9ae1f58be60c3b8943cf3821fdd56e076c45cf02
HaoliangZhou <89777862+HaoliangZhou@users.noreply.github.com>
2025-09-15 15:02:38 +0800

    Delete eval.sh

--- eval.sh
+++
@@ -1,8 +0,0 @@
-export CUDA_VISIBLE_DEVICES=6
-CURRENT_TIME=$(date +%Y-%m-%d_%H:%M:%S)
-LOG_NAME=U2CAR
-OUT_DIR=/checkpoint/model_weights/${LOG_NAME}
-
-python eval.py \
---output_dir ${OUT_DIR} \
-2>&1 | tee ./outputs/${LOG_NAME}_${CURRENT_TIME}.log
